651f2147497e399694cd79935f6e188cbffa8764
Dan Stroot <dan.stroot@gmail.com>
2017-11-05 15:59:28 -0800

    some code cleanup

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,36 +2,36 @@
   <div class="fluid container">
     <div class="form-group form-group-lg panel panel-default">
       <div class="panel-heading">
-        <h3 class="panel-title">Sortbale control</h3>
+        <h3 class="panel-title">Sortable control</h3>
       </div>
       <div class="panel-body">
         <div class = "checkbox">
-          <label><input type = "checkbox" v-model="editable">Enable drag and drop</label>      
+          <label><input type = "checkbox" v-model="editable">Enable drag and drop</label>
         </div>
         <button type="button" class="btn btn-default" @click="orderList">Sort by original order</button>
       </div>
     </div>
 
     <div  class="col-md-3">
-        <draggable class="list-group" element="ul" v-model="list" :options="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false"> 
+        <draggable class="list-group" element="ul" v-model="list" :options="dragOptions" :move="onMove" @start="isDragging=true" @end="isDragging=false">
           <transition-group type="transition" :name="'flip-list'">
-            <li class="list-group-item" v-for="element in list" :key="element.order"> 
+            <li class="list-group-item" v-for="element in list" :key="element.order">
               <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
               {{element.name}}
               <span class="badge">{{element.order}}</span>
-            </li> 
+            </li>
           </transition-group>
       </draggable>
     </div>
 
      <div  class="col-md-3">
-      <draggable element="span" v-model="list2" :options="dragOptions" :move="onMove"> 
+      <draggable element="span" v-model="list2" :options="dragOptions" :move="onMove">
           <transition-group name="no" class="list-group" tag="ul">
-            <li class="list-group-item" v-for="element in list2" :key="element.order"> 
+            <li class="list-group-item" v-for="element in list2" :key="element.order">
               <i :class="element.fixed? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'" @click=" element.fixed=! element.fixed" aria-hidden="true"></i>
               {{element.name}}
               <span class="badge">{{element.order}}</span>
-            </li> 
+            </li>
           </transition-group>
       </draggable>
     </div>
@@ -48,7 +48,7 @@
 
 <script>
 import draggable from 'vuedraggable'
-const message = [ 'vue.draggable', 'draggable', 'component', 'for', 'vue.js 2.0', 'based' , 'on', 'Sortablejs' ]
+const items = [ 'vue.draggable', 'draggable', 'component', 'for', 'vue.js 2.0', 'based' , 'on', 'Sortablejs' ]
 
 export default {
   name: 'hello',
@@ -57,8 +57,9 @@ export default {
   },
   data () {
     return {
-      list: message.map( (name,index) => {return {name, order: index+1, fixed: false}; }),
-      list2:[],
+      list: items.map( (name,index) => {return {name, order: index+1, fixed: false}; }),
+      // turn on to enable second list
+      list2:[{"name": "hello!","order": 1,"fixed": false}],
       editable:true,
       isDragging: false,
       delayedDragging:false
@@ -84,10 +85,10 @@ export default {
       };
     },
     listString(){
-      return JSON.stringify(this.list, null, 2);  
+      return JSON.stringify(this.list, null, 2);
     },
     list2String(){
-      return JSON.stringify(this.list2, null, 2);  
+      return JSON.stringify(this.list2, null, 2);
     }
   },
   watch: {
